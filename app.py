7606a72916224f1291ea594898efeb3173fc5ab3
LEE HANSEO <hslee0824@gmail.com>
2023-05-30 23:05:44 +0900

    test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/greet", methods=["POST"])
+@app.route("/greet")
 def greet():
     name = request.form.get("name")
     return render_template("index_2.html")
