1f013f801d425867f2143309c73e389bdda17eaf
LEE HANSEO <hslee0824@gmail.com>
2023-05-31 15:29:14 +0900

    test

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("templates/index.html")
 
 @app.route("/greet", methods=["POST"])
 def greet():
-    return render_template("index_2.html")
+    return render_template("templates/index_2.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
