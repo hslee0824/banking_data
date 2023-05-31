727e7e519a340ac4927bf62ae8a6c5f991fa32df
LEE HANSEO <hslee0824@gmail.com>
2023-05-31 18:10:54 +0900

    replace app.py with actual file

--- deploy/NOTYET.py
+++ deploy/NOTYET.py
@@ -44,6 +44,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        return render_template("index.html")
         file = request.files.get("file")
         if file is None or file.filename == "":
             return jsonify({"error" : "no file"})
@@ -58,7 +59,7 @@ def index():
 
         except Exception as e:
             return jsonify({"error" : str(e)})
-    return render_template("result.html")
+    return render_template("index2.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
